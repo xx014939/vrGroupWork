183af7c4a6406012d57b94e4626d2af47304a911
James Moody <yk017114@student.reading.ac.uk>
2021-03-20 13:04:30 +0000

    Scripting

--- VR_GROUP_WORK/Assets/Scripts/cameraControls.cs
+++ VR_GROUP_WORK/Assets/Scripts/cameraControls.cs
@@ -28,10 +28,8 @@ public class cameraControls : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
         float mouseX = (Input.mousePosition.x / Screen.width) - 0.5f;
         float mouseY = (Input.mousePosition.y / Screen.height) - 0.5f;
         transform.localRotation = Quaternion.Euler(new Vector4(-1f * (mouseY * 180f), mouseX * 360f, transform.localRotation.z));
-
     }
 }
---
+++ VR_GROUP_WORK/Assets/videoScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class videoScript : MonoBehaviour
+{
+    public Camera fpsCam;
+    public GameObject button;
+    public VideoPlayer video;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        video.Stop();      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+
+            if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit))
+            {
+                if (hit.transform.name == "PlayVideo")
+                {
+                    Debug.Log(hit.transform.name);
+                    video.Play();   
+                }
+            }
+        }
+
+        else if (Input.GetMouseButtonUp(0))
+        {
+
+        }
+    }
+}
