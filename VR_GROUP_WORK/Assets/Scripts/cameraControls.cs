493b4ae9b9eadbe65eee06fbae1b69bb1513ef52
James Moody <yk017114@student.reading.ac.uk>
2021-03-20 17:44:31 +0000

    Interaction

--- VR_GROUP_WORK/Assets/Scripts/cameraControls.cs
+++ VR_GROUP_WORK/Assets/Scripts/cameraControls.cs
@@ -4,7 +4,48 @@ using UnityEngine;
 
 public class cameraControls : MonoBehaviour
 {
+    public Transform playerBody;
 
+    public float xRotation = 0f;
+
+    public float yRotation = 0f;
+
+    Vector2 rotation = new Vector2(0, 0);
+
+    public float mouseSensitivity = 2f;
+
+    void Awake()
+    {
+        Application.targetFrameRate = 30;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rotation.y += Input.GetAxis("Mouse X");
+        rotation.x += -Input.GetAxis("Mouse Y");
+
+        if (rotation.x > 40f)
+        {
+            rotation.x = 40f;
+        }
+
+        else if (rotation.x < -40f)
+        {
+            rotation.x = -40f;
+        }
+
+        transform.eulerAngles = (Vector2)rotation * mouseSensitivity;
+    }
+
+    /*
     public float speed = 20f;
 
     public float speedH = 2.0f;
@@ -32,4 +73,5 @@ public class cameraControls : MonoBehaviour
         float mouseY = (Input.mousePosition.y / Screen.height) - 0.5f;
         transform.localRotation = Quaternion.Euler(new Vector4(-1f * (mouseY * 180f), mouseX * 360f, transform.localRotation.z));
     }
+    */
 }
---
+++ VR_GROUP_WORK/Assets/tapeScript.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class tapeScript : MonoBehaviour
+{
+    public AudioSource audio;
+    public AudioSource audio1;
+    public AudioSource audio2;
+    public AudioSource audio3;
+    public Camera fpsCam;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+
+            if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit))
+            {
+                if (hit.transform.name == "tape1")
+                {
+                    audio.Play();
+                }
+
+                if (hit.transform.name == "tape2")
+                {
+                    audio1.Play();
+                }
+
+                if (hit.transform.name == "tape3")
+                {
+                    audio2.Play();
+                }
+
+                if (hit.transform.name == "tape4")
+                {
+                    audio3.Play();
+                }
+            }
+        }
+    }
+}
