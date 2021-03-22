f530ed40e3d6667c6ee1454fd9843107b5232c11
ChiragVJ <56835688+ChiragVJ@users.noreply.github.com>
2021-03-22 01:57:56 +0000

    lights
    new light switch added for studio,
    fixed lights for control room

--- VR_GROUP_WORK/Assets/Scripts/LightSwitch.cs
+++ VR_GROUP_WORK/Assets/Scripts/LightSwitch.cs
@@ -3,10 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class LightSwitch : MonoBehaviour
-
 {
 
-    public GameObject light1;
+    public GameObject light1; //Declaring the light variables
     private Light light1_comp;
     public GameObject light2;
     private Light light2_comp;
@@ -14,31 +13,66 @@ public class LightSwitch : MonoBehaviour
     private Light light3_comp;
     public GameObject light4;
     private Light light4_comp;
-
-    private bool lightSwitch;
-
-
+    public GameObject LightInstructionsOff;
+    public GameObject LightInstructionsOn;
+    private bool lightSwitch = true;
 
     void Start()
     {
-        light1_comp = light1.GetComponent<Light>();
+        light1_comp = light1.GetComponent<Light>(); //initializing the lights
         light2_comp = light2.GetComponent<Light>();
         light3_comp = light3.GetComponent<Light>();
         light4_comp = light4.GetComponent<Light>();
+        LightInstructionsOff.SetActive(false);
+        LightInstructionsOn.SetActive(false);
 
-        lightSwitch = true;
     }
-    
+
     void OnTriggerStay(Collider other)
     {
-        if (other.tag == "Player" && Input.GetKeyDown(KeyCode.E))
+
+
+
+        if (lightSwitch)
         {
-            light1_comp.enabled = false;
-            light2_comp.enabled = false;
-            light3_comp.enabled = false;
-            light4_comp.enabled = false;
+
+            LightInstructionsOn.SetActive(false);
+            LightInstructionsOff.SetActive(true);
+            if (Input.GetMouseButtonDown(1)) //if the player presses right click
+            {
+
+                light1_comp.enabled = false; //turn lights on 
+                light2_comp.enabled = false;
+                light3_comp.enabled = false;
+                light4_comp.enabled = false;
+                lightSwitch = false;
+
+            }
         }
+        if (!lightSwitch)
+        {
+            LightInstructionsOff.SetActive(false);
+            LightInstructionsOn.SetActive(true);
+            if (Input.GetMouseButtonDown(0)) //if the player presses left click
+            {
+
+                light1_comp.enabled = true; //turn lights on 
+                light2_comp.enabled = true;
+                light3_comp.enabled = true;
+                light4_comp.enabled = true;
+                lightSwitch = true;
+
+
+            }
+        }
+
     }
 
-  
+    void OnTriggerExit(Collider other)
+    {
+
+        LightInstructionsOff.SetActive(false); //remove panel 
+        LightInstructionsOn.SetActive(false);
+
+    }
 }
---
+++ VR_GROUP_WORK/Assets/Scripts/LightSwitchStudio.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightSwitchStudio : MonoBehaviour
+{
+
+    public GameObject light1; //Declaring the light variables
+    private Light light1_comp;
+    public GameObject light2;
+    private Light light2_comp;
+    public GameObject light3;
+    private Light light3_comp;
+    public GameObject light4;
+    private Light light4_comp;
+    public GameObject light5;
+    private Light light5_comp;
+    public GameObject light6;
+    private Light light6_comp;
+    public GameObject LightInstructionsOff;
+    public GameObject LightInstructionsOn;
+    private bool lightSwitch = true;
+
+    void Start()
+    {
+        light1_comp = light1.GetComponent<Light>(); //initializing the lights
+        light2_comp = light2.GetComponent<Light>();
+        light3_comp = light3.GetComponent<Light>();
+        light4_comp = light4.GetComponent<Light>();
+        light5_comp = light5.GetComponent<Light>();
+        light6_comp = light6.GetComponent<Light>();
+        LightInstructionsOff.SetActive(false);
+        LightInstructionsOn.SetActive(false);
+
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+
+
+
+        if (lightSwitch)
+        {
+
+            LightInstructionsOn.SetActive(false);
+            LightInstructionsOff.SetActive(true);
+            if (Input.GetMouseButtonDown(1)) //if the player presses right click
+            {
+
+                light1_comp.enabled = false; //turn lights on 
+                light2_comp.enabled = false;
+                light3_comp.enabled = false;
+                light4_comp.enabled = false;
+                light5_comp.enabled = false;
+                light6_comp.enabled = false;
+                lightSwitch = false;
+
+            }
+        }
+        if (!lightSwitch)
+        {
+            LightInstructionsOff.SetActive(false);
+            LightInstructionsOn.SetActive(true);
+            if (Input.GetMouseButtonDown(0)) //if the player presses left click
+            {
+
+                light1_comp.enabled = true; //turn lights on 
+                light2_comp.enabled = true;
+                light3_comp.enabled = true;
+                light4_comp.enabled = true;
+                light5_comp.enabled = true;
+                light6_comp.enabled = true;
+                lightSwitch = true;
+
+
+            }
+        }
+
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+
+        LightInstructionsOff.SetActive(false); //remove panel 
+        LightInstructionsOn.SetActive(false);
+
+    }
+}
